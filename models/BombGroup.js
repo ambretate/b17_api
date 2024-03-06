709a2eb4e73205700903acb3e7d3db759edede6b
Ambre Tate <ambretate@gmail.com>
2024-03-06 12:12:34 -0500

    seed data added for groups.json file

--- models/BombGroup.js
+++ models/BombGroup.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const BombGroupSchema = new Schema({
-  serialNumber: [{ type: Schema.Types.serialNumber, ref: "b17s" }],
+  groupName: String,
+  bombSquadron: [String],
 });
 
 let Group = mongoose.model("groups", BombGroupSchema);
--- models/BombSquadron.js
+++ models/BombSquadron.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const BombSquadronSchema = new Schema({
-  bombGroup: [String],
+  squadronName: String,
+  serialNumber: [{ type: Schema.Types.serialNumber, ref: "b17s" }],
 });
 
 let Squadron = mongoose.model("squadrons", BombSquadronSchema);
